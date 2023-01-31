0314dd2343358988c6929ddd9e7d19592f1b7b3c
0xdineshkumarsm <105421120+0xdineshkumarsm@users.noreply.github.com>
2023-01-31 12:35:47 +0530

    smaller values(< 2^160)

---
+++ src/RBTLibv2.sol
@@ -0,0 +1,381 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.17;
+
+import "forge-std/console.sol";
+import "./Utils.sol";
+library RedBlackTreeLib {
+    // using LibBitmap for Tree;
+
+    uint24 private constant EMPTY = 0;
+
+    struct Node {
+        uint160 value;
+        bool red;
+        uint24 parent;
+        uint24 left;
+        uint24 right;
+    }
+
+    struct Tree{
+        uint24 root;
+        uint24 totalNodes;
+        mapping (uint => Node) nodes;
+    }
+
+    function size(Tree storage self) internal view returns (uint){
+        return uint256(self.totalNodes);
+    }
+    function getRoot(Tree storage self) internal view returns(uint){
+        return self.nodes[self.root].value;
+    }
+    function getKey(Tree storage self,uint value) internal view returns (uint24) {
+        require(value != EMPTY,"value != EMPTY");
+        uint24 probe = self.root;
+        while(probe != EMPTY){
+            if (value == self.nodes[probe].value){
+                return probe;
+                // break;
+            }
+            else if (value < self.nodes[probe].value){
+                probe = self.nodes[probe].left;
+            } else {
+                probe = self.nodes[probe].right;
+            }
+        }
+        return probe;
+    }
+    function getNode(Tree storage self, uint value) internal view returns (uint _returnKey, uint _parent, uint _left, uint _right, bool _red) {
+        // require(exists(self, value));
+        uint24 key = getKey(self,value);
+        require(key != EMPTY,string.concat("RBT::getNode()# NOT EXISTS ",uint2str(key)));
+        // return(value, self.nodes[key].parent, self.nodes[key].left, self.nodes[key].right, self.nodes[key].red);
+        return(value, self.nodes[self.nodes[key].parent].value, self.nodes[self.nodes[key].left].value, self.nodes[self.nodes[key].right].value, self.nodes[key].red);
+    }
+    function getNodeByIndex(Tree storage self, uint key) internal view returns (uint _returnKey, uint _parent, uint _left, uint _right, bool _red) {
+        // require(exists(self, value));
+        require(key != EMPTY,string.concat("RBT::getNode()# NOT EXISTS ",uint2str(key)));
+        // return(value, self.nodes[key].parent, self.nodes[key].left, self.nodes[key].right, self.nodes[key].red);
+        return(self.nodes[key].value, self.nodes[self.nodes[key].parent].value, self.nodes[self.nodes[key].left].value, self.nodes[self.nodes[key].right].value, self.nodes[key].red);
+    }
+    function exists(Tree storage self, uint value) internal view returns (bool) {
+        return (value != EMPTY) && (getKey(self,value) != EMPTY);
+        // return (value != EMPTY) && self.totalNodes != EMPTY ;
+    }
+    function rotateLeft(Tree storage self, uint24 key) private {
+        uint24 cursor = self.nodes[key].right;
+        uint24 keyParent = self.nodes[key].parent;
+        uint24 cursorLeft = self.nodes[cursor].left;
+        self.nodes[key].right = cursorLeft;
+        if (cursorLeft != EMPTY) {
+            self.nodes[cursorLeft].parent = key;
+        }
+        self.nodes[cursor].parent = keyParent;
+        if (keyParent == EMPTY) {
+            self.root = cursor;
+        } else if (key == self.nodes[keyParent].left) {
+            self.nodes[keyParent].left = cursor;
+        } else {
+            self.nodes[keyParent].right = cursor;
+        }
+        self.nodes[cursor].left = key;
+        self.nodes[key].parent = cursor;
+    }
+    function rotateRight(Tree storage self, uint24 key) private {
+        uint24 cursor = self.nodes[key].left;
+        uint24 keyParent = self.nodes[key].parent;
+        uint24 cursorRight = self.nodes[cursor].right;
+        self.nodes[key].left = cursorRight;
+        if (cursorRight != EMPTY) {
+            self.nodes[cursorRight].parent = key;
+        }
+        self.nodes[cursor].parent = keyParent;
+        if (keyParent == EMPTY) {
+            self.root = cursor;
+        } else if (key == self.nodes[keyParent].right) {
+            self.nodes[keyParent].right = cursor;
+        } else {
+            self.nodes[keyParent].left = cursor;
+        }
+        self.nodes[cursor].right = key;
+        self.nodes[key].parent = cursor;
+    }
+
+    function insertFixup(Tree storage self, uint24 key) private {
+        uint24 cursor;
+        while (key != self.root && self.nodes[self.nodes[key].parent].red) {
+            uint24 keyParent = self.nodes[key].parent;
+            if (keyParent == self.nodes[self.nodes[keyParent].parent].left) {
+                cursor = self.nodes[self.nodes[keyParent].parent].right;
+                if (self.nodes[cursor].red) {
+                    self.nodes[keyParent].red = false;
+                    self.nodes[cursor].red = false;
+                    self.nodes[self.nodes[keyParent].parent].red = true;
+                    key = self.nodes[keyParent].parent;
+                } else {
+                    if (key == self.nodes[keyParent].right) {
+                      key = keyParent;
+                      rotateLeft(self, key);
+                    }
+                    keyParent = self.nodes[key].parent;
+                    self.nodes[keyParent].red = false;
+                    self.nodes[self.nodes[keyParent].parent].red = true;
+                    rotateRight(self, self.nodes[keyParent].parent);
+                }
+            } else { // if keyParent on right side
+                cursor = self.nodes[self.nodes[keyParent].parent].left;
+                if (self.nodes[cursor].red) {
+                    self.nodes[keyParent].red = false;
+                    self.nodes[cursor].red = false;
+                    self.nodes[self.nodes[keyParent].parent].red = true;
+                    key = self.nodes[keyParent].parent;
+                } else {
+                    if (key == self.nodes[keyParent].left) {
+                      key = keyParent;
+                      rotateRight(self, key);
+                    }
+                    keyParent = self.nodes[key].parent;
+                    self.nodes[keyParent].red = false;
+                    self.nodes[self.nodes[keyParent].parent].red = true;
+                    rotateLeft(self, self.nodes[keyParent].parent);
+                }
+            }
+        }
+        self.nodes[self.root].red = false;
+    } 
+
+    function insert(Tree storage self, uint160 value) internal {
+        // console.log("inserting",value);
+        require(value != EMPTY,"value != EMPTY");
+        require(!exists(self,value),"No Duplicates! ");
+        uint24 cursor = EMPTY;
+        uint24 probe = self.root;
+        // print(self);
+
+        while(probe != EMPTY){
+            cursor = probe;
+            if (value < self.nodes[probe].value){
+                probe = self.nodes[probe].left;
+            } else {
+                probe = self.nodes[probe].right;
+            }
+        }
+        
+        uint24 newNodeIdx = ++self.totalNodes;
+        // console.log("newNodeIdx ",newNodeIdx);
+        self.nodes[newNodeIdx] = Node({value:value,red:true,parent: cursor, left: EMPTY, right: EMPTY});
+        if (cursor == EMPTY) {
+            self.root = newNodeIdx;
+        } else if (value < self.nodes[cursor].value){
+            self.nodes[cursor].left = newNodeIdx;
+        } else {
+            self.nodes[cursor].right = newNodeIdx;
+        }
+        // print(self);
+        // console.log("insert ended",value);
+        insertFixup(self, newNodeIdx);
+    }
+
+    function replaceParent(Tree storage self, uint24 a, uint24 b) private {
+        uint24 bParent = self.nodes[b].parent;
+        self.nodes[a].parent = bParent;
+        if (bParent == EMPTY) {
+            self.root = a;
+        } else {
+            if (b == self.nodes[bParent].left) {
+                self.nodes[bParent].left = a;
+            } else {
+                self.nodes[bParent].right = a;
+            }
+        } 
+    }
+    function removeFixup(Tree storage self, uint24 key) private {
+        // console.log("removeFixup()#",key,self.nodes[key].value);
+        uint24 cursor;
+        while (key != self.root && !self.nodes[key].red) {
+        // console.log("removeFixup()# debug 1");
+
+            uint24 keyParent = self.nodes[key].parent;
+            if (key == self.nodes[keyParent].left) {
+                cursor = self.nodes[keyParent].right;
+                if (self.nodes[cursor].red) {
+                    self.nodes[cursor].red = false;
+                    self.nodes[keyParent].red = true;
+                    rotateLeft(self, keyParent);
+                    cursor = self.nodes[keyParent].right;
+                }
+                if (!self.nodes[self.nodes[cursor].left].red && !self.nodes[self.nodes[cursor].right].red) {
+                    self.nodes[cursor].red = true;
+                    key = keyParent;
+                } else {
+                    if (!self.nodes[self.nodes[cursor].right].red) {
+                        self.nodes[self.nodes[cursor].left].red = false;
+                        self.nodes[cursor].red = true;
+                        rotateRight(self, cursor);
+                        cursor = self.nodes[keyParent].right;
+                    }
+                    self.nodes[cursor].red = self.nodes[keyParent].red;
+                    self.nodes[keyParent].red = false;
+                    self.nodes[self.nodes[cursor].right].red = false;
+                    rotateLeft(self, keyParent);
+                    key = self.root;
+                }
+            } else {
+                cursor = self.nodes[keyParent].left;
+                if (self.nodes[cursor].red) {
+                    self.nodes[cursor].red = false;
+                    self.nodes[keyParent].red = true;
+                    rotateRight(self, keyParent);
+                    cursor = self.nodes[keyParent].left;
+                }
+                if (!self.nodes[self.nodes[cursor].right].red && !self.nodes[self.nodes[cursor].left].red) {
+                    self.nodes[cursor].red = true;
+                    key = keyParent;
+                } else {
+                    if (!self.nodes[self.nodes[cursor].left].red) {
+                        self.nodes[self.nodes[cursor].right].red = false;
+                        self.nodes[cursor].red = true;
+                        rotateLeft(self, cursor);
+                        cursor = self.nodes[keyParent].left;
+                    }
+                    self.nodes[cursor].red = self.nodes[keyParent].red;
+                    self.nodes[keyParent].red = false;
+                    self.nodes[self.nodes[cursor].left].red = false;
+                    rotateRight(self, keyParent);
+                    key = self.root;
+                }
+            }
+        }
+        self.nodes[key].red = false;
+    }
+
+    function remove(Tree storage self, uint value) internal {
+        require(value != EMPTY);
+        // require(exists(self, value));
+        uint24 probe;
+        uint24 cursor;
+        uint24 key = getKey(self,value);
+        // console.log("removing ",value,key);
+        if (self.nodes[key].left == EMPTY || self.nodes[key].right == EMPTY) {
+            cursor = key;
+        } else {
+            cursor = self.nodes[key].right;
+            while (self.nodes[cursor].left != EMPTY) {
+                cursor = self.nodes[cursor].left; // 13
+            }
+            // cursor = self.nodes[key].left;
+            // while (self.nodes[cursor].right != EMPTY) {
+            //     cursor = self.nodes[cursor].right; // 13
+            // }
+        }
+        if (self.nodes[cursor].left != EMPTY) {
+            probe = self.nodes[cursor].left;
+        } else {
+            probe = self.nodes[cursor].right;
+        }
+        uint24 yParent = self.nodes[cursor].parent;
+        self.nodes[probe].parent = yParent;
+        // console.log("yParent,probe,cursor,key");
+        // printNodeByIndex(self,yParent);
+        // printNodeByIndex(self,probe);
+        // printNodeByIndex(self,cursor);
+        // printNodeByIndex(self,key);
+        if (yParent != EMPTY) {
+            if (cursor == self.nodes[yParent].left) {
+                self.nodes[yParent].left = probe;
+            } else {
+                self.nodes[yParent].right = probe;
+            }
+        } else {
+            // console.log("debugg ```````````1");
+            self.root = probe;
+            // print(self);
+        }
+        bool doFixup = !self.nodes[cursor].red;
+        if (cursor != key) {
+            replaceParent(self, cursor, key);
+            self.nodes[cursor].left = self.nodes[key].left;
+            self.nodes[self.nodes[cursor].left].parent = cursor;
+            self.nodes[cursor].right = self.nodes[key].right;
+            self.nodes[self.nodes[cursor].right].parent = cursor;
+            self.nodes[cursor].red = self.nodes[key].red;
+            (cursor, key) = (key, cursor);
+        }
+        if (doFixup) {
+            removeFixup(self, probe);
+        }
+        // console.log("a4 doFixUp");
+        // print(self);
+        // console.log("cursor,self.totalNodes",cursor,self.totalNodes);
+        uint24 last = self.totalNodes;
+        Node memory lastNode = self.nodes[last];
+        if (self.nodes[cursor].value != lastNode.value){
+
+            
+            self.nodes[cursor] = lastNode;
+            uint24 lParent = lastNode.parent;
+            // printNodeByIndex(self,last);
+            // console.log("lastNode",lastNode);
+            // console.log("last.parent",last.parent);
+            // console.log("cursor",cursor);
+            if(lastNode.parent != EMPTY){
+                if (self.totalNodes == self.nodes[lParent].left){
+                    self.nodes[lParent].left = cursor;
+                } else {
+                    self.nodes[lParent].right = cursor;
+                }
+            }else {
+                self.root = cursor;
+            }
+            if (lastNode.right != EMPTY){
+                self.nodes[lastNode.right].parent = cursor;
+            }
+            if (lastNode.left != EMPTY){
+                self.nodes[lastNode.left].parent = cursor;
+            }
+            // console.log("b4 delete");
+            // print(self);
+        }
+        delete self.nodes[last];
+        // console.log("self.totalNodes",self.totalNodes);
+        self.totalNodes--;
+    }
+
+    function print(Tree storage self) internal view {
+        console.log("--------- root",self.root," totalNodes",self.totalNodes);
+        uint _size = self.totalNodes;
+        for(uint key;key<=_size;key++){
+            console.log(string.concat(
+                uint2str(key)," ",
+                self.nodes[key].red ? "R" : "B"," ",
+                uint2str(self.nodes[key].parent)," ",
+                uint2str(self.nodes[key].left)," ",
+                uint2str(self.nodes[key].right)," ",
+                uint2str(self.nodes[key].value)," "
+            ));
+        }
+        console.log("------------------");
+    }
+
+    function printNodeByIndex(Tree storage self,uint key) internal view{
+        console.log(string.concat(
+                uint2str(key)," ",
+                self.nodes[key].red ? "R" : "B"," ",
+                uint2str(self.nodes[key].parent)," ",
+                uint2str(self.nodes[key].left)," ",
+                uint2str(self.nodes[key].right)," ",
+                uint2str(self.nodes[key].value)," "
+            ));
+    }
+    function printNode(Tree storage self,uint val) internal view{
+        uint key = getKey(self,val);
+        console.log(string.concat(
+                uint2str(key)," ",
+                self.nodes[key].red ? "R" : "B"," ",
+                uint2str(self.nodes[key].parent)," ",
+                uint2str(self.nodes[key].left)," ",
+                uint2str(self.nodes[key].right)," ",
+                uint2str(self.nodes[key].value)," "
+            ));
+    }
+    
+}
---
+++ test/RBTv2.t.sol
@@ -0,0 +1,161 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity 0.8.17;
+import "forge-std/Test.sol";
+import "src/Utils.sol";
+import "../src/BokkyPooBahsRedBlackTreeLibrary.sol";
+// import "../src/RBTLib.sol";
+import "../src/RBTLibv2.sol";
+contract RedBlackTestv2 is Test {
+    using BokkyPooBahsRedBlackTreeLibrary for BokkyPooBahsRedBlackTreeLibrary.Tree;
+    using RedBlackTreeLib for RedBlackTreeLib.Tree;
+    
+    BokkyPooBahsRedBlackTreeLibrary.Tree BPBtree;
+    RedBlackTreeLib.Tree RBtree;
+
+    uint80 private constant EMPTY = 0;
+
+    // function check(BokkyPooBahsRedBlackTreeLibrary.Tree storage bpb_tree,RedBlackTreeLib.Tree storage rb_tree,uint val) internal {
+    //     // console.log("checking ",val);
+    //     (,uint bpb_p,uint bpb_l,uint bpb_r,bool bpb_c) = bpb_tree.getNode(val);
+    //     (,uint rb_p,uint rb_l,uint rb_r,bool rb_c) = rb_tree.getNode(val);
+
+    //     // require(bpk_val == rb_val,"")
+    //     require(bpb_p==rb_p,string.concat("parent mismatch ",uint2str(val)," ",uint2str(bpb_p)," ",uint2str(rb_p)));
+    //     require(bpb_l==rb_l,string.concat("left mismatch ",uint2str(val)," ",uint2str(bpb_l)," ",uint2str(rb_l)));
+    //     require(bpb_r==rb_r,string.concat("right mismatch ",uint2str(val)," ",uint2str(bpb_r)," ",uint2str(rb_r)));
+    //     require(bpb_c==rb_c,string.concat("color mismatch ",uint2str(val)," ",bool2str(bpb_c)," ",bool2str(rb_c)));
+
+    //     if(bpb_l == EMPTY && bpb_r == EMPTY) return;
+    //     if(bpb_l != EMPTY) check(bpb_tree,rb_tree,bpb_l);
+    //     if(bpb_r != EMPTY) check(bpb_tree,rb_tree,bpb_r);
+
+    // }
+    function equal(BokkyPooBahsRedBlackTreeLibrary.Tree storage bpb_tree,RedBlackTreeLib.Tree storage rb_tree) internal returns(bool){
+        assertEq(bpb_tree.root,rb_tree.getRoot(),"root mismatch");
+        if (bpb_tree.root != EMPTY){
+            // check(bpb_tree,rb_tree,bpb_tree.root);
+            for(uint i = 1;i<= rb_tree.totalNodes;i++){
+                (uint rb_val,uint rb_p,uint rb_l,uint rb_r,bool rb_c) = rb_tree.getNodeByIndex(i);
+                (uint bpb_val,uint bpb_p,uint bpb_l,uint bpb_r,bool bpb_c) = bpb_tree.getNode(rb_val);
+
+                require(bpb_val == rb_val,string.concat("parent mismatch ",uint2str(bpb_val)," : ",uint2str(rb_val)));
+                require(bpb_p==rb_p,string.concat("parent mismatch ",uint2str(bpb_val)," : ",uint2str(rb_val)," :: ",uint2str(bpb_p)," : ",uint2str(rb_p)));
+                require(bpb_l==rb_l,string.concat("left mismatch ",uint2str(bpb_val)," : ",uint2str(rb_val)," :: ",uint2str(bpb_l)," : ",uint2str(rb_l)));
+                require(bpb_r==rb_r,string.concat("right mismatch ",uint2str(bpb_val)," : ",uint2str(rb_val)," :: ",uint2str(bpb_r)," : ",uint2str(rb_r)));
+                require(bpb_c==rb_c,string.concat("color mismatch ",uint2str(bpb_val)," : ",uint2str(rb_val)," :: ",bool2str(bpb_c)," : ",bool2str(rb_c)));
+            }
+        }
+        return true;
+    }
+
+    function insertRBtree(RedBlackTreeLib.Tree storage rb_tree,uint160 val,bool debug) internal returns(uint){
+        uint tree_size = rb_tree.size();
+        uint GAS_BEFORE = gasleft();
+        rb_tree.insert(val);
+        uint GAS_USED = GAS_BEFORE - gasleft();
+        if (debug) console.log(string.concat("Tree size - ",uint2str(tree_size)," | Gas used after inserting -> ",uint2str(val)),GAS_USED);
+        return GAS_USED;
+    }
+    function insertBPBtree(BokkyPooBahsRedBlackTreeLibrary.Tree storage bpb_tree,uint160 val,bool debug) internal returns(uint){
+        uint GAS_BEFORE = gasleft();
+        bpb_tree.insert(val);
+        uint GAS_USED = GAS_BEFORE - gasleft();
+        if (debug) console.log(string.concat(" | Gas used after inserting -> ",uint2str(val)),GAS_USED);
+        return GAS_USED;
+    }
+
+    function removeRBtree(RedBlackTreeLib.Tree storage rb_tree,uint val,bool debug) internal returns(uint){
+        uint tree_size = rb_tree.size();
+        uint GAS_BEFORE = gasleft();
+        rb_tree.remove(val);
+        uint GAS_USED = GAS_BEFORE - gasleft();
+        if (debug) console.log(string.concat("Tree size - ",uint2str(tree_size)," | Gas used after removing -> ",uint2str(val)),GAS_USED);
+        return GAS_USED;
+    }
+    function removeBPBtree(BokkyPooBahsRedBlackTreeLibrary.Tree storage bpb_tree,uint val,bool debug) internal returns(uint){
+        uint GAS_BEFORE = gasleft();
+        bpb_tree.remove(val);
+        uint GAS_USED = GAS_BEFORE - gasleft();
+        if (debug) console.log(string.concat(" | Gas used after removing -> ",uint2str(val)),GAS_USED);
+        return GAS_USED;
+    }
+    uint160[] input_insert;
+    uint160[] input_delete;
+
+    function setUp() public {
+        // input_insert = [8,5,15,12,19,9,13,23,10,1];
+        // input_delete = [19,5,12];
+        // input_insert = [572,475,454];
+        // input_delete = [572,475,454];
+        uint160 temp = 1 ;
+        for (uint i;i<1000;i++){
+            temp = uint160(uint(keccak256(abi.encode(temp))));
+            input_insert.push(temp);
+            input_delete.push(temp);
+        }
+        console.log("setup",input_insert.length,input_delete.length
+        );
+    }
+
+    function test() public {
+        {uint gas_used_RB;uint gas_used_BPB;
+        uint total_gas_used_RB;uint total_gas_used_BPB;
+        console.log("============= inserting ",input_insert.length," elements (BPB,RB) =============");
+        for (uint i ;i<input_insert.length;i++){
+            // bool debug = i == 0 || i ==10 || i == 100 || i == 1000   ? true : false ;
+            bool debug = true  ;
+            gas_used_RB = insertRBtree(RBtree,input_insert[i],false);
+            gas_used_BPB = insertBPBtree(BPBtree,input_insert[i],false);
+
+            if (debug) console.log(string.concat("gas used for inserting ",uint2str(i+1,6),"th item "
+                                    // ,uint2str(input_insert[i])
+                                    ),gas_used_BPB,gas_used_RB , 
+                                    gas_used_BPB > gas_used_RB ? string.concat("-",uint2str(((gas_used_BPB - gas_used_RB) * 100) /  gas_used_BPB),"%") : string.concat("+",uint2str(((gas_used_RB - gas_used_BPB) * 100) /  gas_used_BPB),"%")
+                                    );
+            total_gas_used_RB += gas_used_RB ;
+            total_gas_used_BPB += gas_used_BPB ;
+        }
+            // require(equal(BPBtree,RBtree));
+        console.log(string.concat("Total gas used for inserting ",uint2str(input_insert.length)," items "),total_gas_used_BPB,total_gas_used_RB,
+                    total_gas_used_BPB > total_gas_used_RB ? string.concat("-",uint2str(((total_gas_used_BPB - total_gas_used_RB) * 100) /  total_gas_used_BPB),"%") : string.concat("+",uint2str(((total_gas_used_RB - total_gas_used_BPB) * 100) /  total_gas_used_BPB),"%")
+        );
+        require(equal(BPBtree,RBtree));    
+        // console.log("====================================================");
+        }
+        // RBtree.print();
+
+        {
+        // uint gas_used_RB;uint gas_used_BPB;
+        uint total_gas_used_RB;uint total_gas_used_BPB;
+
+        console.log("============= removing ",input_delete.length," elements (BPB,RB) =============");
+        for (uint i ;i<input_delete.length;i++){
+            // bool debug = i == 0 || i ==10 || i == 100 || i == 1000  ? true : false ;
+            // if(i == 0 || i ==10 || i == 100 || i == 1000) require(equal(BPBtree,RBtree));
+            if(i ==100 ) require(equal(BPBtree,RBtree));
+            bool debug = true;
+            // if (debug) RBtree.printNode(input_delete[i]);
+            uint gas_used_RB = removeRBtree(RBtree,input_delete[i],false);
+            uint gas_used_BPB = removeBPBtree(BPBtree,input_delete[i],false);
+            if (debug) console.log(string.concat("gas used for removing ",uint2str(i+1,6),"th item "
+                                    // ,uint2str(input_delete[i])
+                                    ),gas_used_BPB,gas_used_RB , 
+                                    gas_used_BPB > gas_used_RB ? string.concat("-",uint2str(((gas_used_BPB - gas_used_RB) * 100) /  gas_used_BPB),"%") : string.concat("+",uint2str(((gas_used_RB - gas_used_BPB) * 100) /  gas_used_BPB),"%"));
+            // RBtree.print();
+            total_gas_used_RB += gas_used_RB ;
+            total_gas_used_BPB += gas_used_BPB ;
+            // console.log("checking ");
+        }
+        console.log(string.concat("Total gas used for removing ",uint2str(input_delete.length)," items "),total_gas_used_BPB,total_gas_used_RB,
+                    total_gas_used_BPB > total_gas_used_RB ? string.concat("-",uint2str(((total_gas_used_BPB - total_gas_used_RB) * 100) /  total_gas_used_BPB),"%") : string.concat("+",uint2str(((total_gas_used_RB - total_gas_used_BPB) * 100) /  total_gas_used_BPB),"%")
+        );
+        // RBtree.print();
+        console.log("====================================================");
+        }
+    }
+
+
+
+}
+
